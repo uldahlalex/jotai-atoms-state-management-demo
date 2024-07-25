e7e80904eefb6d042f920c02e02ddc31fe2fcc8f
Alex Uldahl Pedersen <alex@uldahl.dk>
2024-07-25 14:55:16 +0200

    removing some stuff i didn't want anyways

--- src/App.tsx
+++ src/App.tsx
@@ -8,34 +8,29 @@ export const ProductFormAtom = atom<Product>({
     title: ''
 })
 
-export const allProducts = atom<Product[]>([]);
 
 export default function App() {
 
     const [prodForm, setProdForm] = useAtom(ProductFormAtom)
-    const [allProds, setAllProds] = useAtom(allProducts);
 
     return(<>
         <OtherComponent />
         <input onChange={event => {
             setProdForm({...prodForm, title: event.target.value})
         }} />
-        <button onClick={() => {
-            setAllProds([...allProds, {title: prodForm.title}])
-        }}>add</button>
+
     </>)
+
 }
 
 export function OtherComponent() {
 
     const [prodForm, setProdForm] = useAtom(ProductFormAtom)
-    const [prods] = useAtom(allProducts)
 
     return(
         <>
             {JSON.stringify(prodForm)}
 
-            {JSON.stringify(prods)}
         </>
     )
 
